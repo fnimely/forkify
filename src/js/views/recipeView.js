4823a615de33d66b9e8b27365e8bbfff3fb17a65
facsimile419 <dohfacsimile@gmail.com>
2022-06-15 09:29:47 -0700

    numberToFraction test

--- src/js/views/recipeView.js
+++ src/js/views/recipeView.js
@@ -130,7 +130,7 @@ class RecipeView extends View {
           <use href="${icons}#icon-check"></use>
         </svg>
         <div class="recipe__quantity">${
-          new Fraction(ing.quantity).toString() ?? ''
+          new numberToFraction(ing.quantity).toString() ?? ''
         }</div>
         <div class="recipe__description">
           <span class="recipe__unit">${ing.unit}</span>
